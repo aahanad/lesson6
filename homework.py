f1f8caaeb318788207b9d6709c82163639ef526e
aahanad <150878761+aahanad@users.noreply.github.com>
2024-01-23 18:48:11 -0800

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,15 @@
+#Date: 16/1/2024
+# In your class, there are five groups that participated and got prizes in different competitions held in various schools.
+#Your class teacher asks your help to record all the details. Write a program and use a tuple data structure to store each record. The program should ask the user to enter details like groupname, Sizeofthegroup, dateofthecompetition, venue, typeofthemedal. Store it as a tuple.
+ #   The program should ask the user to enter details like groupname, Sizeofthegroup, dateofthecompetition, venue, typeofthemedal. Store it as a tuple.
+#This should be done for all the five groups.
+group1=("Robotians",6,"Nov16","auditorium7","silver")
+group2=("Weston wolverines",4,"June7","hall3","bronze")
+group3=("Alpha gymnastics",8,"March28","theater2","gold")
+group4=("ISP artists",14,"Dec11","auditorium1","bronze")
+group5=("cherry's orchestra",3,"July21","hall1","gold")
+print(group1)
+print(group2)
+print(group3)
+print(group4)
+print(group5)
