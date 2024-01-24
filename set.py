e23d55eb939cacb28dece0bed29cc029c7a265ca
aahanad <150878761+aahanad@users.noreply.github.com>
2024-01-23 18:49:31 -0800

    Update set.py

--- set.py
+++ set.py
@@ -2,3 +2,14 @@ set1={1,2,3,4,5}
 print(set1)
 set1.add(6)
 set1.remove(6)
+setA={1,2,3,4,5}
+setB={4,5,6,7,8}
+#set operations
+#union
+print(setA|setB)
+#intersection
+print(setA&setB)
+#difference
+print(setA-setB)
+#symmetric difference
+print(setA^setB)
